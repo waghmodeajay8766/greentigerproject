e1e98dacd24a1019653f9536d16447dac550b30d
AjayWaghmode <waghmodeajay8766@gmail.com>
2022-05-18 10:09:20 +0530

    naming changes

--- src/app/navall/navall.component.ts
+++ src/app/navall/navall.component.ts
@@ -1,13 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { GreenserviceService } from '../greenservice.service';
-
+import { FormBuilder, Validators } from '@angular/forms';
 import Swal from 'sweetalert2';
- import 'sweetalert2/src/sweetalert2.scss'
+import 'sweetalert2/src/sweetalert2.scss'
 import { MatDialog } from '@angular/material/dialog';
-import { NavonlineComponent } from '../navonline/navonline.component';
 import { DilogComponent } from '../dilog/dilog.component';
 @Component({
   selector: 'app-navall',
@@ -15,83 +11,83 @@ import { DilogComponent } from '../dilog/dilog.component';
   styleUrls: ['./navall.component.css']
 })
 export class NavallComponent implements OnInit {
- 
-  constructor( private fb: FormBuilder, private http: HttpClient,public dialog : MatDialog) { }
+
+  constructor(private fb: FormBuilder, private http: HttpClient, public dialog: MatDialog) { }
   isSubmitted: boolean = false;
 
-  public myForm:any;
+  public myForm: any;
   openDilog(): void {
     const dialogRef = this.dialog.open(DilogComponent, {
-      
-      
+
+
     });
 
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
-      
+
     });
   }
 
   ngOnInit(): void {
 
     this.myForm = this.fb.group({
-      fname: ['', [Validators.required,Validators.pattern('^[a-zA-Z \-\']+')]],
-      email: ['',[Validators.required,Validators.pattern(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)]],
+      fname: ['', [Validators.required, Validators.pattern('^[a-zA-Z \-\']+')]],
+      email: ['', [Validators.required, Validators.pattern(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)]],
       enqtype: ['', Validators.required],
       center: ['', Validators.required],
-      lname: ['', [Validators.required,Validators.pattern('^[a-zA-Z \-\']+')]],
-      mobaile: ['',[Validators.required,Validators.pattern(/^[6-9]\d{9}$/)]],
+      lname: ['', [Validators.required, Validators.pattern('^[a-zA-Z \-\']+')]],
+      mobaile: ['', [Validators.required, Validators.pattern(/^[6-9]\d{9}$/)]],
       enqsource: ['', Validators.required],
       course: ['', Validators.required],
       remark: ['', Validators.required],
-      
+
     })
-    }
-    onClose(){
-      this.myForm.close();
-      
-      Swal.fire({
-        title: "Are you  want to Close!",
-        text: "Do you want to continue",
-        icon: "warning",
-  
-        showCancelButton: true,
-        confirmButtonText: 'Yes',
-        cancelButtonText: 'No',
-      }).then((result) => {
-        if (result.value == true) {
-          this.myForm = false;
-        }
-      })
-    }
+  }
+  onClose() {
+    this.myForm.close();
+
+    Swal.fire({
+      title: "Are you  want to Close!",
+      text: "Do you want to continue",
+      icon: "warning",
+
+      showCancelButton: true,
+      confirmButtonText: 'Yes',
+      cancelButtonText: 'No',
+    }).then((result) => {
+      if (result.value == true) {
+        this.myForm = false;
+      }
+    })
+  }  
 
   onSubmitForm() {
-    
+
     console.log(this.myForm);
     console.log(this.myForm.value);
     this.isSubmitted = true;
-    
-    if(this.myForm.valid){
-    Swal.fire({
-      title: "Thank You",
-      text: "Data Submitted Successfully",
-      icon: "success",
-      confirmButtonText: 'Okay',
 
-    })
-    this.myForm.reset();
-  } else {
-    return;
-  }
+    if (this.myForm.valid) {
+      Swal.fire({
+        title: "Thank You",
+        text: "Data Submitted Successfully",
+        icon: "success",
+        confirmButtonText: 'Okay',
+
+      })
+      this.myForm.reset();
+    } else {
+      return;
+    }
   }
   onSubmit() {
-     if(this.myForm.valid){
-      
+    if (this.myForm.valid) {
+
       console.log(this.myForm);
       console.log(this.myForm.value);
       this.isSubmitted = true;
-      
-     
+
+
       Swal.fire({
         title: "Thank You",
         text: "Data Submitted Successfully",
@@ -107,12 +103,12 @@ export class NavallComponent implements OnInit {
         confirmButtonText: 'Okay'
       })
     }
-    
+
   }
-  get f() {
+  get formControl() {
     return this.myForm.controls;
-    
+
   }
-  
+
 }
 
